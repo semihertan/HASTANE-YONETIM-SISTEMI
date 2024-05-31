d075ec6aa02af45f9d07f4b9e89ee8a9d7ffef5a
semihertan <161311067+semihertan@users.noreply.github.com>
2024-05-31 16:05:27 +0300

    main.py üzerinde değişiklik

--- main.py
+++ main.py
@@ -6,45 +6,58 @@ from Hasta import Hasta
 
 def main():
     try:
-        personel1 = Personel(101, "Ahmet", "Yılmaz", "Bilgi İşlem", 5000)
-        personel2 = Personel(102, "Ayşe", "Kaya", "İnsan Kaynakları", 6000)
+        personel1 = Personel(101, "Nilgün", "Ertan", "Bilgi İşlem", 17500)
+        personel2 = Personel(102, "Hasan", "Kaya", "İnsan Kaynakları", 19000)
 
         # Doktor nesnelerini oluşturma
-        doktor1 = Doktor(103, "Mehmet", "Demir", "Kardiyoloji", 15000, "Kardiyolog", 10, "Acıbadem Hastanesi")
-        doktor2 = Doktor(104, "Elif", "Sarı", "Nöroloji", 16000, "Nörolog", 8, "Memorial Hastanesi")
+        doktor1 = Doktor(215, "Faik", "Duman", "Kardiyoloji", 85000, "Kardiyolog", 10, "Acıbadem Hastanesi")
+        doktor2 = Doktor(312, "Zeynep", "Dirik", "Nöroloji", 76000, "Nörolog", 8, "Bakırköy Devlet Hastanesi")
+        doktor3 = Doktor(146, "Semih", "Ertan", "Göz Hastalıkları", 98700, "Oftalmolog", 12, "Gülhane Eğitim ve Araştırma Hastanesi")
 
         # Hemşire nesnelerini oluşturma
-        hemsire1 = Hemsire(201, "Fatma", "Demir", "Yoğun Bakım", 8000, 40, "Sertifikalı Yoğun Bakım", "Özel Hastane")
-        hemsire2 = Hemsire(202, "Mustafa", "Yılmaz", "Cerrahi", 8500, 36, "Acil Tıp", "Devlet Hastanesi")
+        hemsire1 = Hemsire(245, "Fatma", "Demir", "Yoğun Bakım", 25600, 40, "Sertifikalı Yoğun Bakım", "İzmir Şehir Hastanesi")
+        hemsire2 = Hemsire(296, "Mustafa", "Yılmaz", "Cerrahi", 24750, 36, "Acil Tıp", "Başakşehir Devlet Hastanesi")
+        hemsire3 = Hemsire(133, "Seda", "Şimşek", "Acil", 28000, 55, "Acil Tıp", "Özel Ataköy Hastanesi")
 
         # Hasta nesnelerini oluşturma
-        hasta1 = Hasta(301, "Ali", "Yılmaz", "1980-05-15", "Ateş", "İlaç Kullanımı")
+        hasta1 = Hasta(301, "Emirhan", "Oğuz", "2004-05-15", "Ateş", "İlaç Kullanımı")
         hasta2 = Hasta(302, "Mehmet", "Kara", "1995-10-20", "Baş Ağrısı", "Dinlenme")
+        hasta3 = Hasta(303, "Yusuf", "Çetin", "1997-06-07", "Grip", "Dinlenme")
 
         # Nesneleri yazdırma
-        print("Oluşturulan Personeller:")
+        print("--------------------------------------PERSONELLER----------------------------------------\n")
         print("Personel 1:")
         print(personel1)
         print("\nPersonel 2:")
         print(personel2)
 
-        print("Doktorlar:")
+
+        print("\n----------------------------------------DOKTORLAR----------------------------------------\n")
         print("Doktor 1:")
         print(doktor1)
         print("\nDoktor 2:")
         print(doktor2)
+        print("\nDoktor 3:")
+        print(doktor3)
 
-        print("\nHemşireler:")
+
+        print("\n---------------------------------------HEMŞİRELER----------------------------------------\n")
         print("Hemşire 1:")
         print(hemsire1)
         print("\nHemşire 2:")
         print(hemsire2)
+        print("\nHemşire 3:")
+        print(hemsire3)
 
-        print("\nHastalar:")
+        print("\n----------------------------------------HASTALAR-----------------------------------------\n")
         print("Hasta 1:")
         print(hasta1)
         print("\nHasta 2:")
         print(hasta2)
+        print("\nHasta 3:")
+        print(hasta3)
+        print("\n")
+
 
         pd.set_option('display.max_columns', None)  # Tüm sütunları göster
         pd.set_option('display.max_rows', None)  # Tüm satırları göster
@@ -52,30 +65,42 @@ def main():
         pd.set_option('display.max_colwidth', None)
 
         data = []
-        for obj in [personel1, personel2, doktor1, doktor2, hemsire1, hemsire2, hasta1, hasta2]:
+        for obj in [personel1, personel2, doktor1, doktor2, doktor3, hemsire1, hemsire2, hemsire3, hasta1, hasta2, hasta3]:
             data.append(obj.to_dict())
 
         df = pd.DataFrame(data).fillna(0)
         print(df)
 
-        df['dogum_tarihi'] = pd.to_datetime(df['dogum_tarihi'], errors='coerce')  # Doğum tarihi sütununu datetime yap
-        df_filtered = df[(df['dogum_tarihi'] >= '1990-01-01')]
+        # Seçilen sütunlar
+        selected_columns_df = df[['ad', 'soyad', 'departman', 'maas', 'uzmanlik', 'deneyim_yili', 'hastalik', 'tedavi']]
+        print("\n**ÖZEL DATAFRAME**")
+        print(selected_columns_df)
 
-        print("\nDoğum Tarihi 1990 ve Sonrası Olan Hastalar:")
-        print(df_filtered)
+        # Maaşı 7000 TL üzeri olan personelleri bulup yazdırma
+        maas_7000_ustu = df[df['maas'] > 7000]
+        print("\nMAAŞI 7000 TL ÜZERİ OLAN PERSONELLER:")
+        print(maas_7000_ustu)
 
-        df_filtered_salary = df[df['maas'] > 7000]
-        print("\nMaaşı 7000 TL'nin Üzerinde Olan Personeller:")
-        print(df_filtered_salary)
+        # Doğum tarihi 1990 ve sonrası olan hastaları bulup yazdırma
+        df['dogum_tarihi'] = pd.to_datetime(df['dogum_tarihi'], errors='coerce')
+        dogum_tarihi_1990_sonrasi = df[(df['dogum_tarihi'] >= '1990-01-01') & (df['dogum_tarihi'] != 0)]
+        print("\n**DOĞUM TARİHİ 1990 VE SONRASI OLAN HASTALAR**")
+        print(dogum_tarihi_1990_sonrasi)
 
-        experienced_doctors = df[(df['deneyim_yili'] > 5)]
-        print("\n5 Yıldan Fazla Deneyime Sahip Doktorlar:")
-        print(experienced_doctors)
+        # Hasta adına göre alfabetik sıralanan dataframe
+        hasta_df = df[df['hastalik'] != 0].sort_values(by='ad')
+        print("\n**HASTA ADINA GÖRE SIRALANMIŞ DATAFRAME**")
+        print(hasta_df)
 
-        # Uzmanlık alanlarına göre doktorları gruplandırarak toplam sayıyı hesaplama
-        doctors_grouped_by_specialty = experienced_doctors.groupby('uzmanlik').size()
-        print("\nUzmanlık Alanlarına Göre 5 Yıldan Fazla Deneyime Sahip Doktorların Sayısı:")
-        print(doctors_grouped_by_specialty)
+        # 5 yıldan fazla deneyime sahip doktorların sayısını bulma ve yazdırma
+        doktor_df = df[df['uzmanlik'] != 0]
+        deneyim_5_yil_ustu = doktor_df[doktor_df['deneyim_yili'] > 5]
+        print(f"\n5 YILDAN FAZLA DENEYİME SAHİP DOKTOR SAYISI: {len(deneyim_5_yil_ustu)}")
+
+        # Uzmanlık alanlarına göre gruplandırarak toplam sayıyı hesaplama
+        uzmanlik_gruplari = deneyim_5_yil_ustu.groupby('uzmanlik').size().reset_index(name='sayisi')
+        print("\nUZMANLIK ALANLARINA GÖRE DOKTOR SAYILARI")
+        print(uzmanlik_gruplari)
 
     except Exception as e:
         print(f"Bir hata oluştu: {e}")
